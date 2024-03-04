789cc27fd89be557d626117ec42b1173144ce4a7
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-03-04 20:03:44 -0300

    remade the project without swc and started working on the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import bgShape from './assets/bg.svg'
 import './App.css'
 
 function App() {
@@ -8,18 +7,8 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className=''>dsñfjk</div>
+      <h1 className='h-screen'>Vite + React</h1>
     </>
   )
 }
