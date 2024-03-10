5d8ba2fe5fda050b2f0eac5c61d8be30f823347b
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-03-10 17:31:21 -0300

    grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card.jsx'
-import projectImage from './assets/cardsBg/conway.gif'
-import forestImage from './assets/cardsBg/forest.png'
+import conwayImage from './assets/cardsBg/conway.gif'
+import uxImage from './assets/cardsBg/researcher.png'
+import expenseImage from './assets/cardsBg/expense-tracker.jpg'
+import sessionImage from './assets/cardsBg/sessions.png'
+import aboutImage from './assets/cardsBg/forest.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   const cardsConfig = {
     heroCard: {
@@ -14,22 +17,84 @@ function App() {
       border: '2',
       primaryBg: '#2a116b',
     },
-    projectCard: {
+    projectCardExpenseTracker: {
       height: '712',
       width: '400',
       border: '2', 
-      primaryBg: projectImage,
+      primaryBg: expenseImage,
+      title: 'Desarrollo de producto asistido por IA',
+      fecha: '01-01-2020',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
+    }, 
+    projectCardMedible: {
+      height: '712',
+      width: '400',
+      border: '2', 
+      primaryBg: uxImage,
+      title: 'Experiencia con Medible',
+      fecha: '01-01-2020',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
+    }, 
+    projectCardSessions: {
+      height: '712',
+      width: '400',
+      border: '2', 
+      primaryBg: sessionImage,
+      title: 'Aplicación de productividad en base a la técnica pomodoro',
+      fecha: '01-01-2020',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
+    }, 
+    projectCardConway: {
+      height: '712',
+      width: '400',
+      border: '2', 
+      primaryBg: conwayImage,
+      title: "Conway's Game of life",
+      fecha: '01-01-2020',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
+    },
+    aboutCard: {
+      height: '712',
+      width: '400',
+      border: '2', 
+      primaryBg: aboutImage,
+      title: 'Más sobre mi',
+      fecha: '',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
+    },
+    contactCard: {
+      height: '712',
+      width: '400',
+      border: '2', 
+      primaryBg: '#FFFFFF',
+      title: 'Hablemos',
+      fecha: '01-01-2020',
+      descripcion: 'Lorem ipsum dolor sit amet consectetur. Nam faucibus vel imperdiet elementum non pulvinar quis at. Commodo porttitor varius mauris aliquet nibh ultrices in. Mollis non sapien morbi a. Aliquam et tellus tortor vitae eu.',
+
     }
   }
   
 
   return (
     <>
-      <div className=''>dsñfjk</div>
-      <h1 className=' '>grid container</h1>
-      {/* TODO: Function to calculate height, width and border to pass it into Card */}
-      <Card config={cardsConfig.heroCard} />
-      <Card config={cardsConfig.projectCard} />
+      <div className='grid grid-cols-4 grid-rows-9 gap-4 m-4 md:m-16 max-w-[1280px]'>
+        {/* TODO toggle for description */}
+        {/* TODO Responsiveness: below 750px change layout */}
+        <Card config={cardsConfig.heroCard} className='col-span-4 row-span-3' />
+        <Card config={cardsConfig.projectCardExpenseTracker} className=' row-span-3' />
+        <Card config={cardsConfig.projectCardMedible} className='col-span-3 row-span-2' />
+        <Card config={cardsConfig.projectCardSessions} className='col-span-3' />
+        <Card config={cardsConfig.projectCardConway} className='row-span-3' />
+        <Card config={cardsConfig.aboutCard} className='row-span-3 col-span-2' />
+        <Card config={cardsConfig.contactCard} className='row-span-3' />
+
+      </div>
+      <h1 className=' '>footer</h1>
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,40 +7,33 @@ export default function Card(props) {
     
     // props.config
     const config = props.config;
-    console.log(config)
     
     const innerWidth = config.height-config.border;
     const innerHeight = config.width-config.border;
 
     return <>
     {/* JSX */}
-        <div>
-
-            <div className={`flex items-center justify-center bg-gradient-to-bl from-[#ffffff40] rounded-2xl`} // #000000 00 last two nums are opacity
+        <div className={`shadow-xl ${props.className} h-full w-full`}>
+            <div className={`flex items-end justify-start rounded-2xl relative h-full w-full bg-cover bg-no-repeat`} 
                 style={{
-                    height: `${config.height}px`,
-                    width: `${config.width}px`,
-                }}>
-                {/* first bg -> handles stroke / border */}
-                <div className={`flex items-end justify-start rounded-2xl relative `} 
+                    backgroundImage: `${config.primaryBg.split('')[0] != "#" ? `url(${config.primaryBg})` : ''}`,
+                    backgroundColor: `${config.primaryBg.split('')[0] != "#" ? '' : `${config.primaryBg}99`}`
+                }}
+                >
+                {/* handles purple gradient */}
+                <div
+                    className="absolute inset-0 bg-gradient-to-tr from-[#0d0d0d] via-[#0d0d0de5] via-15% bg-opacity-0 rounded-2xl backdrop-blur-[5px] border-[1px] border-[#474747]"
                     style={{
-                        height: `${innerWidth}px`,
-                        width: `${innerHeight}px`,
-                        backgroundImage: `${config.primaryBg.split('')[0] != "#" ? `url(${config.primaryBg})` : ''}`,
-                        backgroundColor: `${config.primaryBg.split('')[0] != "#" ? '' : `${config.primaryBg}99`}`
+                    // mixBlendMode: 'darken',
                     }}
-                    >
-                    {/* 2nd bg -> handles purple gradient */}
-                    <div
-                        className="absolute inset-0 bg-gradient-to-tr from-[#0d0d0d] via-[#0d0d0d] via-30% bg-opacity-0 rounded-2xl backdrop-blur-[2px]"
-                        style={{
-                        mixBlendMode: 'darken',
-                        }}
-                    >                    </div>
-                    {/* Content here, make sure this comes after the overlay div so it's on top */}
-                    <div className="m-8 z-10 opacity-100">
-                        flkdsañjñdlk
-                    </div>
+                ></div>
+                {/* Content here, make sure this comes after the overlay div so it's on top */}
+                <div className="m-8 z-10 opacity-100">
+                    <h1>{config.title}</h1>
+                    <p>{config.fecha}</p>
+                    {/* TODO toggle for description */}
+                    {/* <p>{config.descripcion}</p> */}
+                    <button>Ver más</button>
                 </div>
             </div>
         </div>
